9f49e70d9407a80e9f23b7ea279077660466e551
Sadaruwansadaru <168360793+Sadaruwansadaru@users.noreply.github.com>
2024-08-20 15:40:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
 const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
 const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+global.owner = process.env.OWNER_NUMBER  || '94762672034' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=OY0DUB5B#PsXyI_yBoYF3pncPdKGm1sWCtDvAo0wkUHKhkOulIuo' ,
